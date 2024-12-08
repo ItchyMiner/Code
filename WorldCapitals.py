20422c76c2b62dc5b0b24fe710f3396d0c670340
Itchy_Laptop <itchyminer@gmail.com>
2024-12-07 23:11:52 -0500

    Deletes empty line

---
+++ WorldCapitals.py
@@ -0,0 +1,5 @@
+User_Input = input("What is the Capital of Sweden?")
+if User_Input == "Stockholm" or "stockholm":
+    print("Yay! You got it right!")
+else:
+    print("You didn't get it :( Better luck next time!")
